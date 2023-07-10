44b0b0e9490eae2566b05e600b6b406793c03146
SidP18 <SidharthPrakasan23@gmail.com>
2023-07-10 17:22:22 -0400

    Secured my personal email as well

--- python_app.py
+++ python_app.py
@@ -2,12 +2,13 @@
 # generate app password
 # create a function to send the email
 from email.message import EmailMessage #Specifies the sender, password, receiver
-from app_info import email_psk
+from app_info import email_password
+from app_info import email_sender
 import ssl
 import smtplib
 
-email_sender = 'SidharthPrakasan23@gmail.com'
-email_psk = email_psk
+email_sndr = 'SidharthPrakasan23@gmail.com'
+email_psk = email_password
 
 email_receiver = 'nohol73687@niback.com'
 
@@ -17,7 +18,7 @@ Hello, When you get this email, you'll know this python email sender is working!
 """
 
 em = EmailMessage()
-em['From'] = email_sender
+em['From'] = email_sndr
 em['To'] = email_receiver
 em['subject'] = subject
 em.set_content(body)
@@ -25,5 +26,5 @@ em.set_content(body)
 context = ssl.create_default_context()
 
 with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp: # used for login and sending the email
-    smtp.login(email_sender, email_psk)
-    smtp.sendmail(email_sender, email_receiver, em.as_string())
+    smtp.login(email_sndr, email_psk)
+    smtp.sendmail(email_sndr, email_receiver, em.as_string())
